4db45093638d01f9a22e51b969aed7ebc756413a
Zhengqiang Liao <lzq666amn@gmail.com>
2025-03-30 19:55:22 +0800

    Initial Commit

---
+++ Ling/LingApp.swift
@@ -0,0 +1,17 @@
+//
+//  LingApp.swift
+//  Ling
+//
+//  Created by Zane Liao on 30/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct LingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
